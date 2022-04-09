99f3e75e0da420b3dd0f53e250a206820e30f30f
Robert Soriano <sorianorobertc@gmail.com>
2022-04-08 20:58:48 -0700

    log meta url

--- example/test.ts
+++ example/test.ts
@@ -3,7 +3,7 @@ import * as path from 'https://deno.land/std@0.134.0/path/mod.ts';
 // console.log(import.meta.main)
 // console.log(import.meta.url)
 
-const filePath = path.fromFileUrl(new URL("icon.png", Deno.mainModule));
-console.log(filePath)
-
+// const filePath = path.fromFileUrl(new URL("icon.png", Deno.mainModule));
+// console.log(filePath)
+console.log(import.meta.url)
 // console.log(Deno.execPath())
