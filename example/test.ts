4fde4b2bd178f9b4f1c329b9667460f344558046
Robert Soriano <sorianorobertc@gmail.com>
2022-04-08 20:26:54 -0700

    log dirname

--- example/test.ts
+++ example/test.ts
@@ -1,4 +1,7 @@
-
+import * as path from 'https://deno.land/std@0.134.0/path/mod.ts';
 console.log(Deno.mainModule)
 console.log(import.meta.main)
 console.log(import.meta.url)
+const __dirname = path.dirname(path.fromFileUrl(import.meta.url));
+
+console.log(__dirname)
