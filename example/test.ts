235723a72ed9e16065029d6af30862b0661a6eb7
Robert Soriano <sorianorobertc@gmail.com>
2022-04-08 20:40:29 -0700

    test new url

--- example/test.ts
+++ example/test.ts
@@ -2,9 +2,11 @@ import * as path from 'https://deno.land/std@0.134.0/path/mod.ts';
 // console.log(Deno.mainModule)
 // console.log(import.meta.main)
 // console.log(import.meta.url)
-// const __dirname = path.dirname(path.fromFileUrl(import.meta.url));
+const __dirname = path.fromFileUrl(new URL('../example/icon.png', import.meta.url));
 
-// console.log(__dirname)
+console.log(__dirname)
 
-const filePath = path.fromFileUrl(new URL("icon.png", Deno.cwd()));
-console.log(filePath)
+// const filePath = path.fromFileUrl(new URL("icon.png", Deno.cwd()));
+// console.log(filePath)
+
+// console.log(Deno.execPath())
